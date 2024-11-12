e7e6acf931ea2d46abcc27020b442505c49c0aa8
HoangArt <daylamotcaiemailok@gmail.com>
2024-11-12 17:20:51 +0700

    Add files via upload

---
+++ feedback.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Đóng góp ý kiến | Herculis</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="icon" type="image/x-icon" href="img/"> <!-- Tạo icon -->
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="p-3 bg-white">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="index.php" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                    <p>Herculis</p>
+                </a>
+
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+                </ul>
+
+            
+
+                <div class="text-end">
+                    <a href="login.php" class="btn btn-outline-primary me-2">Đăng nhập</a>
+                    <a href="signup.html" class="btn btn-primary">Đăng ký</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <h1>Đóng góp ý kiến</h1>
+</body>
+</html>
